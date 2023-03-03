7e3b26832afa9d9048966e77205aa61c1b99cf20
Joel Boychuk <joelboychuk@gmail.com>
2023-03-03 20:41:36 +0100

    initial commit

---
+++ components/ToDoInput.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NewToDo from "./NewToDo";
+
+export default function ToDoInput() {
+  return (
+    <div className="input-container cfs">
+      <div className="todo-input">
+        <NewToDo />
+      </div>
+    </div>
+  );
+}
